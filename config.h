6f431b8a1250acf1a75a655d92a996565c21bb6b
kuring <361118339@qq.com>
2021-08-19 20:02:59 +0800

    patch morecolor

--- config.h
+++ config.h
@@ -16,9 +16,10 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#B48EAD" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeMid] = { "#eeeeee", "#222222" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
