95b6e4617b04c9cac5f87e842072eb3e7c4de7a5
kuring <361118339@qq.com>
2021-08-19 19:33:43 +0800

    support color emoji, change fonts

--- config.h
+++ config.h
@@ -6,7 +6,9 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"SauceCodePro Nerd Font Mono:size=16",
+	"LXGW WenKai Mono:size=16",
+	"JoyPixels:pixelsize=16:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
